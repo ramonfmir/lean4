396794403e03e83e6fcfd6cc264b5bcaa674b7da
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-08-17 20:15:29 +0200

    More tests

--- tests/lean/floatMantissaExponent.lean
+++ tests/lean/floatMantissaExponent.lean
@@ -1,3 +1,7 @@
 #eval Float.me 0.0
+#eval Float.me (-0.0)
 #eval Float.me 1.0
 #eval Float.me (-1.0)
+#eval Float.me 12341234312312313212313213123
+#eval Float.me (1.0 / 0.0)
+#eval Float.me (0.0 / 0.0)
